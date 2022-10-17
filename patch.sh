3dd5a43645db8432cee76ca49bb91e1cb378a551
lonelil <51315646+lonelil@users.noreply.github.com>
2022-10-17 17:47:10 +0800

    added font

--- patch.sh
+++ patch.sh
@@ -2,12 +2,19 @@
 
 mkdir dist
 
-for Pack in $(ls Packs) 
-do
+for Fonts in $(ls Fonts); do
     unzip enmity.ipa
-    cp -rf Packs/${Pack}/* Payload/Discord.app/assets/
-    zip -r dist/${Pack}cord.ipa Payload
+    cp -rf Fonts/${Fonts}/* Payload/Discord.app/
+    zip -r dist/${Fonts}cord.ipa Payload
     rm -rf Payload
 done
 
-
+for Pack in $(ls Packs); do
+    for Fonts in $(ls Fonts); do
+        unzip enmity.ipa
+        cp -rf Fonts/${Fonts}/* Payload/Discord.app/
+        cp -rf Packs/${Pack}/* Payload/Discord.app/assets/
+        zip -r dist/${Fonts}cord-${Pack}.ipa Payload
+        rm -rf Payload
+    done
+done
